fa44ed0fc8e6ce4fd086778eaa6cf1f8bdb35bb0
CardReign Developer <louisma31@gmail.com>
2026-02-05 21:57:47 -0500

    test(android): temporarily disable Plaid SDK to isolate build failure

--- utils/plaid-sdk.native.ts
+++ utils/plaid-sdk.native.ts
@@ -1,4 +1,15 @@
-import { create, LinkExit, LinkSuccess, open } from 'react-native-plaid-link-sdk';
+// Plaid SDK temporarily disabled for Android build testing
+// Original: import { create, LinkExit, LinkSuccess, open } from 'react-native-plaid-link-sdk';
 
-export { create, LinkExit, LinkSuccess, open };
+export type LinkSuccess = { publicToken: string; metadata: unknown };
+export type LinkExit = { errorCode?: string; errorMessage?: string; metadata: unknown };
 
+export const create = (config: unknown) => {
+    console.warn('Plaid Link is currently disabled. This feature will be enabled in a future update.');
+    return Promise.resolve();
+};
+
+export const open = () => {
+    console.warn('Plaid Link is currently disabled. This feature will be enabled in a future update.');
+    return Promise.resolve();
+};
